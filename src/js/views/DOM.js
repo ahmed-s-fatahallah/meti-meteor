4881f72f1d5cce931ba3ef083c32d3be85b5841e
AhmedSaeed2305 <115148623+AhmedSaeed2305@users.noreply.github.com>
2023-02-19 18:14:37 +0200

    refactor: Change the OOP classes to functions for consistaency

---
+++ src/js/views/DOM.js
@@ -0,0 +1,8 @@
+const DOM = {
+  mainSectionEl: document.querySelector(".main"),
+  tilesArry: Array.from(document.querySelectorAll(".tile")),
+  yellowMeteor: document.querySelector(".yellow"),
+  blueMeteor: document.querySelector(".blue"),
+};
+
+export default DOM;
