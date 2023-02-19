44fac0e37d75abd5a6637e1829bb496a2302815b
AhmedSaeed2305 <115148623+AhmedSaeed2305@users.noreply.github.com>
2023-02-19 19:09:17 +0200

    refactore: change the OOP class to functions for consistency

--- src/js/views/meteorsView.js
+++ src/js/views/meteorsView.js
@@ -1,34 +1,25 @@
-import view from "./view";
+import dom from "./DOM";
 
-class meteorsView extends view {
-  _isHolding;
-  getMeteors() {
-    const blueMeteor = this._blueMeteor;
-    const yellowMeteor = this._yellowMeteor;
-    return [blueMeteor, yellowMeteor];
-  }
-  #holdingMouse() {
-    this._isHolding = true;
-  }
-  releaseMouse(handler, targetTile) {
-    document.addEventListener("mouseup", (e) => {
-      this._isHolding = false;
-      if (e.target === this._blueMeteor || e.target === this._yellowMeteor) {
-        targetTile(e.target, this._firstTile);
-      }
-      handler(this._blueMeteor, this._yellowMeteor);
-    });
-  }
-  AddHandlerMove(handler) {
-    document.addEventListener("mousedown", this.#holdingMouse.bind(this));
+let isHolding;
 
-    document.addEventListener("mousemove", (e) => {
-      if (!this._isHolding) return;
-      const meteor = e.target.closest(".meteor");
-      if (!meteor) return;
-      handler(meteor, e);
-    });
-  }
-}
+export const mouseDown = function () {
+  document.addEventListener("mousedown", () => {
+    isHolding = true;
+  });
+};
 
-export default new meteorsView();
+export const moveMouse = function (handler) {
+  document.addEventListener("mousemove", (e) => {
+    if (!isHolding) return;
+    const meteor = e.target.closest(".meteor");
+    if (!meteor) return;
+    handler(e, meteor);
+  });
+};
+
+export const mouseUp = function (handler) {
+  document.addEventListener("mouseup", () => {
+    isHolding = false;
+    handler(dom.blueMeteor, dom.yellowMeteor);
+  });
+};
