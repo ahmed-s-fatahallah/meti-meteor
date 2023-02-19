f9973f9dcd73489a6410a202ed15bd2eccb7b908
AhmedSaeed2305 <115148623+AhmedSaeed2305@users.noreply.github.com>
2023-02-19 19:35:23 +0200

    refactore: import new models and connect new veiw functions

--- src/js/controler.js
+++ src/js/controler.js
@@ -1,11 +1,12 @@
 import * as model from "./model";
-import meteorsView from "./views/meteorsView";
-import view from "./views/view";
+import * as meteor from "./views/meteorsView";
+import dom from "./views/DOM";
 
 const Init = function () {
-  model.meteorsInitPos(...meteorsView.getMeteors());
-  meteorsView.AddHandlerMove(model.moveMeteors);
-  meteorsView.releaseMouse(model.restoreMeteorInitPos, model.targetTile);
+  model.meteorsInitPos(dom.blueMeteor, dom.yellowMeteor);
+  meteor.mouseDown();
+  meteor.moveMouse(model.moveMeteors);
+  meteor.mouseUp(model.restoreMeteorInitPos, model.meteorsDroppedTiles);
 };
 
 Init();
