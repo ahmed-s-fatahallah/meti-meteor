25090951fff18087953a3a43d9897dbc35d5190a
AhmedSaeed2305 <115148623+AhmedSaeed2305@users.noreply.github.com>
2023-02-20 20:23:54 +0200

    feat: select yellow meteor inner div

--- src/js/views/DOM.js
+++ src/js/views/DOM.js
@@ -2,6 +2,7 @@ const DOM = {
   mainSectionEl: document.querySelector(".main"),
   tilesArry: Array.from(document.querySelectorAll(".tile")),
   yellowMeteor: document.querySelector(".yellow"),
+  innerYellowMeteor: document.querySelector(".yellow-inner"),
   blueMeteor: document.querySelector(".blue"),
 };
 
