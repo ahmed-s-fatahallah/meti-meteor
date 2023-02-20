cf41283be57bb5686c8bc976f387303b3f9fb456
AhmedSaeed2305 <115148623+AhmedSaeed2305@users.noreply.github.com>
2023-02-20 19:36:24 +0200

    refactor: refactore the meteorsDroppedTiles function

--- src/js/model.js
+++ src/js/model.js
@@ -13,8 +13,52 @@ export const meteorsInitPos = function (blueMeteor, yellowMeteor) {
   meteorsPos = meteorsObj;
 };
 
+export const moveMeteors = function (e, meteor) {
+  meteor.style.top =
+    parseInt(getComputedStyle(meteor).top) + e.movementY + "px";
+  meteor.style.left =
+    parseInt(getComputedStyle(meteor).left) + e.movementX + "px";
+};
+
+export const restoreMeteorInitPos = function (blueMeteor, yellowMeteor) {
+  setTimeout(() => {
+    blueMeteor.style.top = meteorsPos.blueMeteor.topPos;
+    blueMeteor.style.left = meteorsPos.blueMeteor.leftPos;
+    yellowMeteor.style.top = meteorsPos.yellowMeteor.topPos;
+    yellowMeteor.style.left = meteorsPos.yellowMeteor.leftPos;
+  }, 3000);
+};
+
 export const meteorsDroppedTiles = function (meteor) {
+  let tilesHit;
   meteorData = meteor.getBoundingClientRect();
+
+  const sidesCoordinatesArry = sidesCoordinates(meteorData);
+  const angleCoordinatesArry = anglesCoordinates(meteorData);
+  const center = centerCoordinates(meteorData);
+  const middleHit = document
+    .elementsFromPoint(...center)
+    .filter((el) => el.classList[0] === "tile");
+  if (middleHit.length === 0) return;
+  if (Array.from(meteor.classList).includes("blue")) {
+    const sidesHit = sidesCoordinatesArry.map((c) =>
+      document.elementFromPoint(...c)
+    );
+
+    tilesHit = sidesHit.concat(middleHit);
+  } else {
+    const outerHit = sidesCoordinatesArry
+      .concat(angleCoordinatesArry)
+      .map((c) => document.elementFromPoint(...c))
+      .filter((el) => el !== null && el.classList[0] === "tile");
+    tilesHit = outerHit.concat(middleHit);
+  }
+
+  const droppedZone = Array.from(new Set(Array.from(tilesHit)));
+  return droppedZone;
+};
+
+const sidesCoordinates = function (meteorData) {
   const [topBoundryX, topBoundryY] = [
     meteorData.left + meteorData.width / 2,
     meteorData.top - 1,
@@ -31,29 +75,46 @@ export const meteorsDroppedTiles = function (meteor) {
     meteorData.right,
     meteorData.bottom - meteorData.height / 2,
   ];
-  const el1 = document.elementFromPoint(topBoundryX, topBoundryY);
-  const el2 = document.elementFromPoint(bottomBoundryX, bottomBoundryY);
-  const el3 = document.elementFromPoint(leftBoundryX, leftBoundryY);
-  const el4 = document.elementFromPoint(rightBoundryX, rightBoundryY);
 
-  const droppedZone = Array.from(new Set([el1, el2, el3, el4]));
-  const results = droppedZone.filter((el) => el.classList[0] === "tile");
-  if (results.length === 0) return;
-  return results;
+  return [
+    [topBoundryX, topBoundryY],
+    [bottomBoundryX, bottomBoundryY],
+    [leftBoundryX, leftBoundryY],
+    [rightBoundryX, rightBoundryY],
+  ];
 };
 
-export const moveMeteors = function (e, meteor) {
-  meteor.style.top =
-    parseInt(getComputedStyle(meteor).top) + e.movementY + "px";
-  meteor.style.left =
-    parseInt(getComputedStyle(meteor).left) + e.movementX + "px";
+const anglesCoordinates = function (meteorData) {
+  const [leftTopCornerX, leftTopCornerY] = [
+    meteorData.left + 72,
+    meteorData.top + 72,
+  ];
+  const [rightTopCornerX, rightTopCornerY] = [
+    meteorData.right - 72,
+    meteorData.top + 72,
+  ];
+  const [leftBottomCornerX, leftBottomCornerY] = [
+    meteorData.left + 72,
+    meteorData.bottom - 72,
+  ];
+  const [rightBottomCornerX, rightBottomCornerY] = [
+    meteorData.right - 72,
+    meteorData.bottom - 72,
+  ];
+
+  return [
+    [leftTopCornerX, leftTopCornerY],
+    [rightTopCornerX, rightTopCornerY],
+    [leftBottomCornerX, leftBottomCornerY],
+    [rightBottomCornerX, rightBottomCornerY],
+  ];
 };
 
-export const restoreMeteorInitPos = function (blueMeteor, yellowMeteor) {
-  setTimeout(() => {
-    blueMeteor.style.top = meteorsPos.blueMeteor.topPos;
-    blueMeteor.style.left = meteorsPos.blueMeteor.leftPos;
-    yellowMeteor.style.top = meteorsPos.yellowMeteor.topPos;
-    yellowMeteor.style.left = meteorsPos.yellowMeteor.leftPos;
-  }, 1000);
+const centerCoordinates = function (meteorData) {
+  const [centerX, centerY] = [
+    meteorData.left + meteorData.width / 2,
+    meteorData.top + meteorData.height / 2,
+  ];
+
+  return [centerX, centerY];
 };
