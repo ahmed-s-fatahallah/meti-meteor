0edd144c8a7ae0eb6e1b9d3d13730107b6eae526
AhmedSaeed2305 <115148623+AhmedSaeed2305@users.noreply.github.com>
2023-02-21 18:43:59 +0200

    feat: connect the new turns manager functions

--- src/js/controler.js
+++ src/js/controler.js
@@ -1,12 +1,18 @@
 import * as model from "./model";
 import * as meteor from "./views/meteorsView";
+import * as tiles from "./views/tilesView";
+import * as turns from "./views/turnsView";
 import dom from "./views/DOM";
 
 const Init = function () {
   model.meteorsInitPos(dom.blueMeteor, dom.yellowMeteor);
   meteor.mouseDown();
   meteor.moveMouse(model.moveMeteors);
-  meteor.mouseUp(model.restoreMeteorInitPos, model.meteorsDroppedTiles);
+  meteor.mouseUp(
+    model.restoreMeteorInitPos,
+    model.meteorsDroppedTiles,
+    model.turnManager
+  );
 };
 
 Init();
