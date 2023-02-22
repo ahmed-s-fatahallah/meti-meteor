578e23f13dae910d64bc6659513c246902d4e257
AhmedSaeed2305 <115148623+AhmedSaeed2305@users.noreply.github.com>
2023-02-22 17:15:21 +0200

    feat: Implement tiles health mech

--- src/js/views/tilesView.js
+++ src/js/views/tilesView.js
@@ -1,10 +1,20 @@
-export const tilesStyling = function (tiles, health) {
+import dom from "./DOM";
+
+export const tilesStyling = function (tiles, meteor) {
   if (!tiles) return;
   tiles.forEach((t) => {
-    t.style.borderColor = "#FFD700";
-    t.innerHTML = `<span class="tile-text">${
-      health >= 1 ? health : "destroyed"
-    }</span>`;
+    let tileHealth = Number(t.firstElementChild.textContent);
+    if (meteor === dom.yellowMeteor) {
+      t.firstElementChild.textContent = tileHealth - 3;
+    } else {
+      t.firstElementChild.textContent = tileHealth - 1;
+    }
+    if (+t.firstElementChild.textContent === 2) t.style.borderColor = "#FFD700";
+    if (+t.firstElementChild.textContent === 1) t.style.borderColor = "#FF0000";
+    if (+t.firstElementChild.textContent <= 0) {
+      t.firstElementChild.textContent = "Destroyed";
+      t.style.visibility = "hidden";
+      t.classList.add("destroyed");
+    }
   });
-  return health - 1;
 };
