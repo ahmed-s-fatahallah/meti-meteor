e1cfe47ab85beaadf68e30c95ff7badabcd13abb
AhmedSaeed2305 <115148623+AhmedSaeed2305@users.noreply.github.com>
2023-02-22 17:43:31 +0200

    feat: query the middle tile element

--- src/js/views/DOM.js
+++ src/js/views/DOM.js
@@ -1,6 +1,7 @@
 const DOM = {
   mainSectionEl: document.querySelector(".main"),
   tilesArry: Array.from(document.querySelectorAll(".tile")),
+  middleTile: document.querySelector(".middle"),
   yellowMeteor: document.querySelector(".yellow"),
   innerYellowMeteor: document.querySelector(".yellow-inner"),
   blueMeteor: document.querySelector(".blue"),
