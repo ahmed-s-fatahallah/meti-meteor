1c14b78f9a1a01e1231a3426002a84ae34a00843
AhmedSaeed2305 <115148623+AhmedSaeed2305@users.noreply.github.com>
2023-02-23 16:06:11 +0200

    fix: Remove display flex styling by js

--- src/js/views/popUpView.js
+++ src/js/views/popUpView.js
@@ -3,12 +3,10 @@ import dom from "./DOM";
 export const popUpStyling = function (loseCondition, turnsCount) {
   if (loseCondition) {
     dom.popUp.showModal();
-    dom.popUp.style.display = "flex";
     dom.popUp.firstChild.textContent = "YOU LOST 😞😔";
   }
   if (turnsCount <= 0 && !loseCondition) {
     dom.popUp.showModal();
-    dom.popUp.style.display = "flex";
     dom.popUp.firstChild.textContent = "YOU WON 🎉🎉";
     dom.restartBtn.style.backgroundColor = "green";
   }
