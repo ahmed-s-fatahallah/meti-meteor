8c80462650adb89ed521ac4871cffecf416ae264
AhmedSaeed2305 <115148623+AhmedSaeed2305@users.noreply.github.com>
2023-02-25 14:41:25 +0200

    fix: Move the destoyed class styling from js to css

--- src/js/views/tilesView.js
+++ src/js/views/tilesView.js
@@ -3,7 +3,7 @@ import dom from "./DOM";
 export const tilesStyling = function (tiles, meteor) {
   if (!tiles) return;
   tiles.forEach((t) => {
-    let tileHealth = Number(t.firstElementChild.textContent);
+    let tileHealth = +t.firstElementChild.textContent;
     if (meteor === dom.yellowMeteor) {
       t.firstElementChild.textContent = tileHealth - 3;
     } else {
@@ -13,7 +13,6 @@ export const tilesStyling = function (tiles, meteor) {
     if (+t.firstElementChild.textContent === 1) t.style.borderColor = "#FF0000";
     if (+t.firstElementChild.textContent <= 0) {
       t.firstElementChild.textContent = "Destroyed";
-      t.style.visibility = "hidden";
       t.classList.add("destroyed");
     }
   });
