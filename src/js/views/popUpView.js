6206e34cee96e6d15925cfd68fce9e9379e3c351
AhmedSaeed2305 <115148623+AhmedSaeed2305@users.noreply.github.com>
2023-02-25 14:40:18 +0200

    fix: Fix the bug with popUp restart btn

--- src/js/views/popUpView.js
+++ src/js/views/popUpView.js
@@ -2,18 +2,18 @@ import dom from "./DOM";
 
 export const popUpStyling = function (loseCondition, turnsCount) {
   if (loseCondition) {
-    dom.popUp.showModal();
     dom.popUp.firstChild.textContent = "YOU LOST 😞😔";
+    dom.popUp.showModal();
   }
   if (turnsCount <= 0 && !loseCondition) {
-    dom.popUp.showModal();
     dom.popUp.firstChild.textContent = "YOU WON 🎉🎉";
     dom.restartBtn.style.backgroundColor = "#009100";
     dom.restartBtn.style.borderColor = "rgba(255, 0, 0, 0)";
+    dom.popUp.showModal();
   }
-  dom.restartBtn.addEventListener("click", (e) => {
-    e.preventDefault();
+};
+export const restart = function () {
+  dom.restartBtn.addEventListener("click", () => {
     dom.popUp.close();
-    dom.popUp.style.display = "none";
   });
 };
