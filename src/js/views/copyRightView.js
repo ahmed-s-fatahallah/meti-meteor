f259e3fdce297c227f2f8ae4cccaefdfb6252178
AhmedSaeed2305 <115148623+AhmedSaeed2305@users.noreply.github.com>
2023-02-27 21:03:50 +0200

    Updates

--- src/js/views/copyRightView.js
+++
@@ -1,5 +0,0 @@
-import dom from "./DOM";
-
-export const renderCurrentYear = function (year) {
-  dom.copyRight.textContent = year();
-};
--- src/js/views/rulesSectionView.js
+++
@@ -1,8 +0,0 @@
-import dom from "./DOM.js";
-
-export const startPlaying = function () {
-  dom.startPlayingBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-    dom.gameSectionEl.scrollIntoView();
-  });
-};
