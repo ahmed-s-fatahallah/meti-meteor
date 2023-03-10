138fc7d794d59014a6189a1dacc4318f365b9b43
AhmedSaeed2305 <115148623+AhmedSaeed2305@users.noreply.github.com>
2023-03-10 18:28:32 +0200

    refactor: Move some code to its own file

--- src/js/controler.js
+++ src/js/controler.js
@@ -6,14 +6,13 @@ import * as turns from "./views/turnsView";
 import * as container from "./views/meteorsContainerView";
 import { renderCurrentYear } from "./views/copyRightView";
 import * as popUp from "./views/popUpView";
-import * as start from "./views/rulesSectionView";
+import * as start from "./views/heroSectionView";
 import * as helpers from "./helpers";
 
 //  INIT FUNCTION
 const init = function () {
   renderCurrentYear(helpers.GET_CURRENT_YEAR);
   container.renderMeteors(model.meteorsHTML);
-  model.meteorsInitPos(dom.meteorHolderEl.firstChild);
   game.mouseDown(model.getInitPos);
   game.mouseMove(model.moveMeteors);
   game.mouseUp(
@@ -23,25 +22,10 @@ const init = function () {
     model.loseConditoin
   );
   turns.TurnsCount(model.turnsCount);
-  start.startPlaying();
+  start.heroSectionHandlers();
   game.backToTop();
   model.isBtnVisible(dom.heroSectionEl, dom.topBtn);
   popUp.restart(model.restart);
   game.resetGame(model.restart);
 };
-
 init();
-
-const rulesEl = document.querySelector(".rules");
-document.querySelector(".rules-btn").addEventListener("click", (e) => {
-  e.preventDefault();
-  rulesEl.showModal();
-});
-document.querySelector(".close_rules--btn").addEventListener("click", (e) => {
-  e.preventDefault();
-  rulesEl.close();
-});
-document.querySelector(".play-btn").addEventListener("click", (e) => {
-  e.preventDefault();
-  window.scrollTo(0, document.body.scrollHeight);
-});
